322d673f848a5f9f51c8b09cab6a041259efda94
Gregor Kaczmarczyk <aggreggator@gmail.com>
2014-03-09 12:10:05 +0100

    category-products

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,22 @@ $(function () {
         // you could render the template like this
 
         dust.render('productTpl', productsData, function (err, res) {
-            $('#products').append(res);
+            $('#content').append(res);
         });
+    });
+
+    // hover on product images
+    $( '#content' ).on( 'mouseenter mouseout', '#products img[data-hover-src]', function() {
+        console.log('clicked');
+        var original = $(this).attr('src');
+        var replacement = $(this).data('hover-src');
+        $(this).attr('src', replacement).data('hover-src', original);
+        //$(this).css('background-color', '#f00');
+    });
+
+
+    $( '#content' ).on( 'click', '#products img', function() {
+        console.log('clicked');
+        $(this).css('background-color', '#f00');
     });
 });
