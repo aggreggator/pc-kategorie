9058664594ccbf385cd33918de773797ed696660
Gregor Kaczmarczyk <aggreggator@gmail.com>
2014-03-09 17:37:31 +0100

    additional info

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@ $(function () {
 
         var productsData = data;
         // set up and compile the dust.js template
-        var productTpl = dust.compile($('#product-tpl').html(), 'productTpl');
+        var productTpl = dust.compile($('#category-product-tpl').html(), 'productTpl');
         dust.loadSource(productTpl);
 
         // render the template
@@ -16,14 +16,17 @@ $(function () {
     });
 
     // hover on product images
-    $( '#content' ).on( 'mouseover mouseout', '.category-product', function() {
+    $( '#content' ).on( 'mouseenter mouseleave', '.category-product', function() {
         // hover image
         var categoryProductImage = $(this).find('img.product-image');
         var categoryProductImageOriginal = categoryProductImage.attr('src');
         var categoryProductImageHover = categoryProductImage.data('hover-src');
         categoryProductImage.attr('src', categoryProductImageHover).data('hover-src', categoryProductImageOriginal);
 
-
+        // show additional info#
+        var categoryProductInfo = $(this).find('.info');
+        //categoryProductInfo.fadeToggle();
+        categoryProductInfo.fadeToggle();
 //        var original = $(this).attr('src');
 //        var replacement = $(this).data('hover-src');
 //        $(this).attr('src', replacement).data('hover-src', original);
