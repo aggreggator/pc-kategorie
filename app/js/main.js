272ae1b6250f3f1c5cfb4c82efaa68df0ccde9da
Gregor Kaczmarczyk <aggreggator@gmail.com>
2014-03-09 13:39:22 +0100

    category product

--- app/js/main.js
+++ app/js/main.js
@@ -2,32 +2,31 @@
 $(function () {
     'use strict';
 
-    $.getJSON('products.json', function (data) {
+    $.getJSON('json/category-products.json', function (data) {
 
         var productsData = data;
-        // Set up and compile the Dust.js template
+        // set up and compile the dust.js template
         var productTpl = dust.compile($('#product-tpl').html(), 'productTpl');
         dust.loadSource(productTpl);
 
-        // you could render the template like this
-
+        // render the template
         dust.render('productTpl', productsData, function (err, res) {
             $('#content').append(res);
         });
     });
 
     // hover on product images
-    $( '#content' ).on( 'mouseenter mouseout', '#products img[data-hover-src]', function() {
-        console.log('clicked');
-        var original = $(this).attr('src');
-        var replacement = $(this).data('hover-src');
-        $(this).attr('src', replacement).data('hover-src', original);
-        //$(this).css('background-color', '#f00');
-    });
+    $( '#content' ).on( 'mouseover mouseout', '.category-product', function() {
+        // hover image
+        var categoryProductImage = $(this).find('img.product-image');
+        var categoryProductImageOriginal = categoryProductImage.attr('src');
+        var categoryProductImageHover = categoryProductImage.data('hover-src');
+        categoryProductImage.attr('src', categoryProductImageHover).data('hover-src', categoryProductImageOriginal);
 
 
-    $( '#content' ).on( 'click', '#products img', function() {
-        console.log('clicked');
-        $(this).css('background-color', '#f00');
+//        var original = $(this).attr('src');
+//        var replacement = $(this).data('hover-src');
+//        $(this).attr('src', replacement).data('hover-src', original);
     });
+
 });
